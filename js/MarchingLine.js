05c8fbdd35a4096bfced85bde14a3d32fbba07b3
Yi-wen LIN <bongiovi015@gmail.com>
2015-02-23 21:39:51 +0000

    Update modules class
    Signed-off-by: Yi-wen LIN <bongiovi015@gmail.com>

--- js/MarchingLine.js
+++ js/MarchingLine.js
@@ -15,6 +15,7 @@ function MarchingLine(pos, dir, color, bmp) {
 	this.startPos = vec3.clone(this.pos);
 	this.dir      = dir;
 	this.color    = hexToRgb(color);
+	this.color    = {r:0, g:0, b:0};
 
 	this.speed    = vec3.clone(dir);
 	this.bmp      = bmp;
@@ -58,8 +59,8 @@ p.draw = function() {
 	var shadowOffset = 1.0;
 	var n = vec3.create();
 	for(var i=1; i<noise; i++) {
-		vec3.scale(this.shadows, i, n);
-		vec3.add(n, this.pos);
+		vec3.scale(n, this.shadows, i);
+		vec3.add(n, n, this.pos);
 		var alpha = (1 - Math.sin(i/noise * Math.PI * .5)) * 30;
 		if(this.bmp.getPixel(n[0], n[1]).a != 255)
 			this.bmp.delayMixPixel(n[0], n[1], this.color.r*shadowOffset, this.color.g*shadowOffset, this.color.b*shadowOffset, alpha);
@@ -71,7 +72,6 @@ p.checkHit = function() {
 	var x = Math.floor(this.pos[0]);
 	var y = Math.floor(this.pos[1]);
 
-	// console.log( x, y );
 	if(x == this.lastX && y == this.lastY) return false;
 	this.lastX = x;
 	this.lastY = y;
@@ -85,34 +85,34 @@ p.checkHit = function() {
 
 
 p.getSpwanLines = function() {
-	var diff = vec3.subtract(this.pos, this.startPos);
+	var diff = vec3.subtract(this.pos, this.pos, this.startPos);
 	if(vec3.length(diff) < MIN_SPWAN_WIDTH) return [];
 
 	var v1 = vec3.create();
-	vec3.scale(diff, random(.2, .8), v1 );
-	vec3.add(v1, this.startPos);
+	vec3.scale(v1, diff, random(.2, .8));
+	vec3.add(v1, v1, this.startPos);
 
 	var v2 = vec3.create();
-	vec3.scale(diff, random(.2, .8), v2 );
-	vec3.add(v2, this.startPos);
+	vec3.scale(v2, diff, random(.2, .8));
+	vec3.add(v2, v2, this.startPos);
 
 
 	var m = mat4.create();
 	var thetaOffset = .05;
 	mat4.identity(m);
 	var theta = Math.random() > .5 ? random(-thetaOffset, thetaOffset) + Math.PI*.5 : random(-thetaOffset, thetaOffset) - Math.PI*.5;
-	mat4.rotateZ(m, theta);
-	var d1 = vec3.create(this.dir);
-	mat4.multiplyVec3(m, d1);
+	mat4.rotateZ(m, m, theta);
+	var d1 = vec3.clone(this.dir);
+	vec3.transformMat4(d1, d1, m);
 
 	mat4.identity(m);
 	var theta = Math.random() > .5 ? random(-thetaOffset, thetaOffset) + Math.PI*.5 : random(-thetaOffset, thetaOffset) - Math.PI*.5;
-	mat4.rotateZ(m, theta);
-	var d2 = vec3.create(this.dir);
-	mat4.multiplyVec3(m, d2);
+	mat4.rotateZ(m, m, theta);
+	var d2 = vec3.clone(this.dir);
+	vec3.transformMat4(d2, d2, m);
 
 
 	return [{v:v1, d:d1}, {v:v2, d:d2}];
 };
 
-model.exports = MarchingLine();
+module.exports = MarchingLine;
--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 //	LIBS
 require("./libs/bongiovi-min.js");
 var Scheduler = bongiovi.Scheduler;
+var random = function(min, max) { return min + Math.random() * ( max - min); }	
 
 //	IMPORTS
 var Bitmap = require("./Bitmap.js");
+var MarchingLine = require("./MarchingLine.js");
 
 (function() {
 	Main = function() {
@@ -27,12 +29,47 @@ var Bitmap = require("./Bitmap.js");
 
 		this.bmp = new Bitmap(this.canvas);
 		console.log(this.bmp);
+		this._isStopped = false;
+
+		this.lines = [];
+
+		for(var i=0; i<5; i++) {
+			var line = new MarchingLine(vec3.fromValues(random(0, W), random(0, H), 0), vec3.fromValues(random(-1, 1), random(-1, 1), 0), null, this.bmp);
+			this.lines.push(line);	
+		}
 
 		Scheduler.addEF(this, this.loop);
 	};
 
 
 	p.loop = function() {
+		if(this._isStopped) return;
+
+		for(var j=0; j<3; j++) {
+			for(var i=0; i<this.lines.length;i++) {
+				var line = this.lines[i];
+				if(line.died) continue;
+				line.walk();
+				if(!line.checkHit()) line.draw();
+				else {
+					line.died = true;
+					var newLines = line.getSpwanLines();
+					for(var i=0; i<newLines.length; i++) {
+						var nl = new MarchingLine(newLines[i].v, newLines[i].d, null, this.bmp);
+						this.lines.push(nl);	
+					}
+				}
+			}
+		}
+
+		var len = this.lines.length;
+		while(len--) {
+			var l = this.lines[len];
+			if(l.died) 	this.lines.splice(len, 1)
+		}
+
+
+		this.bmp.update();
 	};
 
 
